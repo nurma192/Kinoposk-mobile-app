dc837d904fd63d0db4d079652b985d2d3919720f
Dinmukhammed Zhuman <dinmukhammed.zhuman2004@gmail.com>
2024-10-15 22:23:08 +0500

    Dima

--- app/src/main/java/com/assignmentsmobile/assignment_2/MainActivity.kt
+++ app/src/main/java/com/assignmentsmobile/assignment_2/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
