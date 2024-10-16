6a038078618472aa4da0ba7b5b5220c1a0aaa596
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2024-10-16 21:30:05 +0600

    added skillcinemanavhost, divided main activity file with onboardingscreen and also added file for SkillCinemaDestinations

---
+++ app/src/main/java/com/assignmentsmobile/assignment_2/SkillCinemaDestinations.kt
@@ -0,0 +1,2 @@
+package com.assignmentsmobile.assignment_2
+
---
+++ app/src/main/java/com/assignmentsmobile/assignment_2/SkillCinemaNavHost.kt
@@ -0,0 +1,35 @@
+package com.assignmentsmobile.assignment_2
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun SkillCinemaNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "valid",
+        modifier = modifier
+    ) {
+//        composable(
+//            route = SingleAccount.routeWithArgs,
+//            arguments = SingleAccount.arguments,
+//            deepLinks = SingleAccount.deepLinks
+//        ) { navBackStackEntry ->
+//            val accountType =
+//                navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+//            SingleAccountScreen(accountType)
+//        }
+    }
+}
+
+//fun NavHostController.navigateSingleTopTo(route: String) =
+//    this.navigate(route) { launchSingleTop = true }
+//
+//private fun NavHostController.navigateToSingleAccount(accountType: String) {
+//    this.navigateSingleTopTo("${SingleAccount.route}/$accountType")
+//}
