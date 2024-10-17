0340509b9936f08b5892cd636becfbdbd6775d03
Zhomart <zturtan@gmail.com>
2024-10-18 00:30:03 +0600

    corrected size of AppHeader

--- app/src/main/java/com/assignmentsmobile/assignment_2/ui/components/AppHeader.kt
+++ app/src/main/java/com/assignmentsmobile/assignment_2/ui/components/AppHeader.kt
@@ -2,7 +2,10 @@ package com.assignmentsmobile.assignment_2.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -21,11 +24,13 @@ fun AppHeader(){
     TopAppBar(
         modifier = Modifier
             .padding(start = 10.dp)
-            .background(Color.Black),
+            .fillMaxWidth()
+            .background(Color.White),
         title = {
             Image(
                 painter = painterResource(id = R.drawable.ic_skillcinema),
                 contentDescription = "Icon_SkillCinema",
+                modifier = Modifier.width(120.dp).height(18.dp)
                 )
         }
 
