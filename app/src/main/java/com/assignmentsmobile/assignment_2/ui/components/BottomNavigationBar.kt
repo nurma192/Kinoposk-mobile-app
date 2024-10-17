b6fb30b59cb39a15c3b25602f605f30fc665228e
Zhomart <zturtan@gmail.com>
2024-10-17 19:16:09 +0600

    displayed bottomnavbar with bottombarpages list

--- app/src/main/java/com/assignmentsmobile/assignment_2/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/assignmentsmobile/assignment_2/ui/components/BottomNavigationBar.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.BottomAppBar
@@ -20,7 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.assignmentsmobile.assignment_2.R
+import com.assignmentsmobile.assignment_2.bottomBarPages
 
 @Composable
 @Preview(showBackground = true)
@@ -40,38 +38,24 @@ fun BottomNavigationBar() {
 
         )
     {
-     Row (
-           modifier = Modifier.fillMaxWidth(),
-         verticalAlignment = Alignment.CenterVertically,
-         horizontalArrangement = Arrangement.Center
-     ) {
-         Image(
-             modifier = Modifier.clickable {  },
-             painter = painterResource(id = R.drawable.ic_home),
-             contentDescription = "Icon Home"
-         )
-         Spacer(modifier = Modifier.width(56.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(50.dp, Alignment.CenterHorizontally),
 
-         Image(
-             modifier = Modifier.clickable {  },
-             painter = painterResource(id = R.drawable.ic_search),
-             contentDescription = "Icon Search"
-         )
-         Spacer(modifier = Modifier.width(56.dp))
+            ) {
+            bottomBarPages.forEach { page ->
+                Image(
+                    modifier = Modifier.clickable {
 
-         Image(
-             modifier = Modifier.clickable {  },
-             painter = painterResource(id = R.drawable.ic_profile),
-             contentDescription = "Icon Profile"
-         )
+                    },
+                    painter = painterResource(id = page.image),
+                    contentDescription = "Icon Home"
+                )
+            }
 
-     }
-
-
-
+        }
     }
-
-
 }
 
 
