963351a01a0d4b61ac4e9d01895c7d7eb13f3e45
Dinmukhammed Zhuman <dinmukhammed.zhuman2004@gmail.com>
2024-10-31 14:46:29 +0500

    Versted listPage with lazyverticalGrid

--- app/src/main/java/com/assignmentsmobile/assignment_2/ui/pages/ListPage/ListPage.kt
+++ app/src/main/java/com/assignmentsmobile/assignment_2/ui/pages/ListPage/ListPage.kt
@@ -1,15 +1,21 @@
 package com.assignmentsmobile.assignment_2.ui.pages.ListPage
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,6 +24,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
@@ -28,13 +36,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.assignmentsmobile.assignment_2.R
+import com.assignmentsmobile.assignment_2.ui.components.RatingView
 
 @Preview(showBackground = true)
 @Composable
 fun ListPage() {
     var rowMaxWidth by remember { mutableStateOf(0.dp) }
 
-    Column(modifier = Modifier.padding(top = 30.dp)) {
+    Column(modifier = Modifier.padding(top = 30.dp).background(Color.Blue),
+        horizontalAlignment = Alignment.CenterHorizontally) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -56,10 +66,54 @@ fun ListPage() {
                     fontSize = 14.sp
                 )
             )
-            Spacer(modifier = Modifier.width(rowMaxWidth/34))
+            Spacer(modifier = Modifier.width(rowMaxWidth / 34))
         }
-        LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxWidth().background(Color.Green).padding(horizontal = 70.dp),
+        ) {
+            items(6) { index ->
+                Column(
+                    modifier = Modifier.fillMaxWidth().clickable { },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(width = 111.dp, height = 156.dp)
+                            .clip(shape = RoundedCornerShape(4.dp))
+                            .padding(top = 6.dp, end = 6.dp, bottom = 6.dp)
+                            .background(Color.Red),
+                        contentAlignment = Alignment.TopEnd
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxHeight(),
+                            verticalArrangement = Arrangement.SpaceBetween,
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(text = "7.8")
+                        }
+                    }
+                    Text(
+                        text = "Hello",
+                        modifier = Modifier.padding(top = 8.dp),
+                        style = TextStyle(
+                            color = Color(0xff272727),
+                            fontFamily = FontFamily(Font(R.font.graphik_regular)),
+                            fontSize = 14.sp
+                        )
+                    )
+                    Text(
+                        text = "NoHello",
+                        modifier = Modifier.padding(top = 2.dp),
+                        style = TextStyle(
+                            color = Color(0xff838390),
+                            fontFamily = FontFamily(Font(R.font.graphik_regular)),
+                            fontSize = 12.sp
+                        )
+                    )
+                }
+            }
         }
     }
 }
