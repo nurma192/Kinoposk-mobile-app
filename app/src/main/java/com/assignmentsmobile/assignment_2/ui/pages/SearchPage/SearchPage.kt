3f9beec854ae9976a5843c894ce8dc9a21a24354
nurma192 <nurmakhanbetkenes@gmail.com>
2024-12-03 12:55:37 +0500

    added searchFilmByKeyword Api and repo

--- app/src/main/java/com/assignmentsmobile/assignment_2/ui/pages/SearchPage/SearchPage.kt
+++ app/src/main/java/com/assignmentsmobile/assignment_2/ui/pages/SearchPage/SearchPage.kt
@@ -1,6 +1,7 @@
 package com.assignmentsmobile.assignment_2.ui.pages.SearchPage
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,6 +16,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,7 +39,7 @@ fun SearchPage(
 ) {
     Scaffold(
         topBar = {
-            SearchBar(onFilterClicked)
+            SearchBar(onFilterClicked, onSubmit = { Log.d("searchFilm",onFilterClicked.toString()) })
         }
     ) { innerPaddingSearchPage ->
         //if else kosy kerek list bar zhogyna
@@ -65,7 +70,7 @@ fun SearchPage(
             item {
                 FilmViewInSearch()
             }
-            item{
+            item {
                 Spacer(modifier = Modifier.padding(bottom = 80.dp))
             }
         }
@@ -73,7 +78,7 @@ fun SearchPage(
 }
 
 @Composable
-fun FilmViewInSearch(){
+fun FilmViewInSearch() {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(16.dp)
